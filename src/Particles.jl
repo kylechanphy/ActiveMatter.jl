beca77baf2c55c0ad4d036d4eed0160554ea3c6f
Kyle Chan <kylewingchan@gmail.com>
2022-03-14 00:00:06 +0800

    particles strunt

--- src/Particles.jl
+++ src/Particles.jl
@@ -21,7 +21,7 @@ mutable struct Particle <: AbstractParicles
 
     collide::Int
 end
-Particle(x0 = 0.0, y0 = 0.0, v0 = 0, ϕ0 = 0.0) = Particle(SV(x0, y0), v0 * SV(cos(ϕ0), sin(ϕ0)), SV(x0, y0), 0)
+# Particle(x0 = 0.0, y0 = 0.0, v0 = 0, ϕ0 = 0.0) = Particle(SV(x0, y0), v0 * SV(cos(ϕ0), sin(ϕ0)), SV(x0, y0), 0)
 Particle(pos::AbstractArray, v0 = 0, ϕ0 = 0.0) = Particle(SV(pos[1], pos[2]), v0 * SV(cos(ϕ0), sin(ϕ0)), SV(pos[1], pos[2]), 0)
 
 
